e72e10aec752301957c54e3e3abe63ba8d0a7e59
Angus Tasker <atasker2@gmail.com>
2015-08-18 09:33:05 -0400

    shortened platform views again

--- cacti.js
+++ cacti.js
@@ -1,7 +1,7 @@
 function changeContent() {
 
   var liveUsers = { text: "Live Users (60 min delay)", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=71&rra_id=5&graph_width=1500&graph_height=200&graph_nolegend='>"};
-  var platformViews = { text: "Platform views (past 24 hours)", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=106&rra_id=2&graph_width=1500&graph_height=200&graph_nolegend='>"};
+  var platformViews = { text: "Platform views", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=106&rra_id=2&graph_width=1500&graph_height=200&graph_nolegend='>"};
   var apacheProcs = { text: "Apache processes", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=120&rra_id=0&graph_width=1500&graph_height=200&graph_nolegend='>"};
 
   var myArray = [liveUsers, platformViews, apacheProcs];
