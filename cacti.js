d7490226471a654ca39e39cc694caf3b0ff38f57
Angus Tasker <atasker2@gmail.com>
2015-08-28 11:23:00 -0400

    temp for visitation

--- cacti.js
+++ cacti.js
@@ -4,12 +4,15 @@ function changeContent() {
   var platformViews = { text: "Platform views", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=106&rra_id=2&graph_width=1500&graph_height=200&graph_nolegend='>"};
   var apacheProcs = { text: "Apache processes", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=120&rra_id=0&graph_width=1500&graph_height=200&graph_nolegend='>"};
 
-  var myArray = [liveUsers, platformViews, apacheProcs];
+  // Removed liveUsers & platformViews for guest visit
+  // Put both back in to return to normal
+  var myArray = [apacheProcs];
 
   var sample = Math.floor(Math.random() * myArray.length) + 0;
 
-  document.getElementById("header-container").innerHTML = myArray[sample].text;
-  document.getElementById("graph-container").innerHTML = myArray[sample].image;
+  // To return to normal replace myArray[0] with myArray[sample] to access all graphs
+  document.getElementById("header-container").innerHTML = myArray[0].text;
+  document.getElementById("graph-container").innerHTML = myArray[0].image;
 
   setTimeout(changeContent, 10000);
 
