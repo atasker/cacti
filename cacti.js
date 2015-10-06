46c3a22ef33659fd51d481dea091219a1fee3802
Angus Tasker <atasker2@gmail.com>
2015-10-06 16:08:35 -0400

    attending to latency issue when loading

--- cacti.js
+++ cacti.js
@@ -19,22 +19,14 @@ $(document).ready(function(){
 
   setInterval(function () {
     var sample = frameArray[++i % len];
-    iframe.attr('src', sample);
+    var next = iframe.attr('src', sample);
+    var loaded = next.load();
+    loaded.show();
   }, 10000);
 
+ // setInterval(function () {
+ //   var sample = frameArray[++i % len];
+ //   iframe.attr('src', sample);
+ // }, 10000);
+
 });
-
-//function changeContent() {
-
-  //var liveUsers = { text: "Live Users (60 min delay)", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=71&rra_id=5&graph_width=1500&graph_height=200&graph_nolegend='>"};
-  //var platformViews = { text: "Platform views", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=106&rra_id=2&graph_width=1500&graph_height=200&graph_nolegend='>"};
-  //var apacheProcs = { text: "Apache processes", image: "<img id='graph' src='http://cacti.playrific.com/graph_image.php?local_graph_id=120&rra_id=0&graph_width=1500&graph_height=200&graph_nolegend='>"};
-  //var myArray = [liveUsers, platformViews, apacheProcs];
-  //var sample = Math.floor(Math.random() * myArray.length) + 0;
-  //document.getElementById("header-container").innerHTML = myArray[sample].text;
-  //document.getElementById("graph-container").innerHTML = myArray[sample].image;
-  //setTimeout(changeContent, 10000);
-
-//}
-
-//window.onload = changeContent;
