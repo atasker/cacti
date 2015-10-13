04eba7621bec8fc9cd366913aceb6fd9329b0009
Angus Tasker <atasker2@gmail.com>
2015-10-13 14:22:36 -0400

    changed 12-16 to 2-5

--- cacti.js
+++ cacti.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
   var popMedia = "http://test1.playrific.com/page_views/page_views.cgi";
   var allTime = "http://test1.playrific.com/all_time_views/views.cgi";
   var allTimeGender = "http://test1.playrific.com/all_time_gender/views.cgi";
+  var twoToFive = "http://test1.playrific.com/ages_2_5/views.cgi";
   var fiveToEight = "http://test1.playrific.com/ages_5_8/views.cgi";
   var eightToTwelve = "http://test1.playrific.com/ages_8_12/views.cgi";
-  var twelveToSixteen = "http://test1.playrific.com/ages_12_16/views.cgi";
   var topNewcomers = "http://test1.playrific.com/top_newcomers/views.cgi";
   var liveUsers = "http://test1.playrific.com/graphs/graph1.cgi";
   var platformViews = "http://test1.playrific.com/graphs/graph2.cgi";
   var apacheProcs = "http://test1.playrific.com/graphs/graph3.cgi";
 
   var frameArray = [popMedia, allTime, allTimeGender, fiveToEight,
-                    eightToTwelve, twelveToSixteen, topNewcomers, liveUsers,
+                    eightToTwelve, twoToFive, topNewcomers, liveUsers,
                     platformViews, apacheProcs];
   var len = frameArray.length;
   var iframe = $('#frame');
